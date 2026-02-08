8d16a1cca5c5b99155594ca8ce46fd4ea60e83cd
Mobisafrankline <mobisafranklineo@gmail.com>
2026-02-08 14:23:16 +0300

    Update vite config and package.json for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/OceanAirExpress', 
-})
+  base: mode === "gh-pages" ? "/OceanAirExpress/" : "/",
+}))
